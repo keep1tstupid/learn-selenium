7763dba469cf94d568962428bb188a0c12cfc29f
keepitstupidsimple <alexandra.globa@gmail.com>
2018-05-14 21:40:26 +0300

    Initial commit

---
+++ learn-selenium/SearchPage.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace learn_selenium
+{
+    class SearchPage
+    {
+        private IWebDriver _driver;
+
+        public SearchPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+
+
+        public IWebElement SearchField
+        {
+            get
+            {
+                return _driver.FindElement(By.Id("lst-ib"));
+            }
+        }
+
+        public IWebElement SearchButton
+        {
+            get
+            {
+                return _driver.FindElement(By.Name("btnK"));
+            }
+        }
+
+        public IList<IWebElement> SearchResults
+        {
+            get {
+                return _driver.FindElements(By.ClassName("r"));
+            }
+        }
+
+
+        public void OpenPage()
+        {
+            _driver.Navigate().GoToUrl("https://www.google.ru");
+        }
+
+        public void FindText(string text)
+        {
+            SearchField.SendKeys(text);
+            SearchField.SendKeys(Keys.Enter);
+        }
+
+        public int SearchResultsCount()
+        {
+            return SearchResults.Count();
+        }
+    } 
+}
--- learn-selenium/UnitTest1.cs
+++ learn-selenium/UnitTest1.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 
 namespace learn_selenium
 {
     [TestClass]
     public class UnitTest1
     {
+        static IWebDriver chromeDriver;
+
+        [AssemblyInitialize]
+        public static void SetUp(TestContext context) {
+            chromeDriver = new ChromeDriver(@"C:\Users\User\source\repos\learn-selenium\learn-selenium\packages\Selenium.Chrome.WebDriver.2.37\driver");
+        }
+
+    
         [TestMethod]
-        public void TestMethod1()
+        public void SimpleGoogleSearch()
         {
+            SearchPage googleSearch = new SearchPage(chromeDriver);
+            googleSearch.OpenPage();
+            googleSearch.FindText("pepe");
+            Assert.AreEqual(10, googleSearch.SearchResultsCount());
         }
     }
 }
